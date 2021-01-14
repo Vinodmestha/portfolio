8c57329175ad2dc2498b10099a293783f2d0eb0b
ashwini <ashwimestha@gmail.com>
2021-01-14 11:50:27 +0530

    update code

---
+++ index.js
@@ -0,0 +1,19 @@
+const readMore = () =>{
+
+    let btn1 = document.getElementById('btn').innerHTML= 
+    'Here i\'m created the similar website like facebook-clone. It\'s a self-created website. I amke as soon as possible.' ;
+
+
+    btn=style.None;
+}
+
+
+
+
+const openmenu = () =>{
+    document.getElementById('close').style.width="150px";
+}
+
+const closemenu = () =>{
+    document.getElementById('close').style.width="0px";
+}
